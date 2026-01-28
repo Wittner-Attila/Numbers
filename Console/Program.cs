d30d7f0072299555f3aa2929a5cab9bd39596734
Wittner Attila <wiattila8@gmail.com>
2026-01-28 10:05:17 +0100

    1, 2 feladat

---
+++ Console/Program.cs
@@ -0,0 +1,67 @@
+﻿int[] table = TableGame();
+
+string tableString = "";
+table.ToList().ForEach(t => tableString += t);
+
+Console.WriteLine(tableString);
+
+
+int[] TableGame()
+{
+    int[,] table = new int[3, 3] ;
+
+    for (int i = 0; i < 3; i++)
+    {
+        int[] line = ReadListFromConsole();
+        for (int j = 0; j < 3; j++)
+        {
+            table[i,j] = line[j];
+        }
+    }
+
+    if (!IsTableCorrect(table)) return [-1];
+    return [table[0, 0], table[0, 2], table[2, 0], table[2, 2],];
+}
+
+
+bool IsTableCorrect(int[,] array)
+{
+    for (int i = 0;i < 3; i++)
+    {
+        if (array[i,0] + array[i,2] != array[i,1]) return false;
+        if (array[0, i] + array[2, i] != array[1, i]) return false;
+    }
+    return true;
+}
+
+void WriteArrayToConsole(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+
+static int[] ReadListFromConsole()
+{
+    List<int> value;
+    do
+    {
+        value = new List<int>();
+        Console.WriteLine($"Enter a list of 3 numbers(1,2,3)");
+        string input = Console.ReadLine();
+        foreach (var item in input.Split(','))
+        {
+            if (String.IsNullOrEmpty(item)) continue;
+            int val;
+            int.TryParse(item, out val);
+            value.Add(val);
+        }
+        ;
+    } while (value.Count != 3);
+    return value.ToArray();
+}
---
+++ Desktop/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Desktop;
+
+public partial class MainWindow : Window
+{
+    public ObservableCollection<int> Numbers { get; private set; } =
+        new ObservableCollection<int> { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        DataContext = this;
+    }
+
+    public void AClicked(object sender, RoutedEventArgs e)
+    {
+        Numbers[0]++;
+        Numbers[1]++;
+        Numbers[3]++;
+        Numbers[4]++;
+    }
+
+    public void BClicked(object sender, RoutedEventArgs e)
+    {
+        Numbers[1]++;
+        Numbers[2]++;
+        Numbers[4]++;
+        Numbers[5]++;
+    }
+
+    public void CClicked(object sender, RoutedEventArgs e)
+    {
+        Numbers[3]++;
+        Numbers[4]++;
+        Numbers[6]++;
+        Numbers[7]++;
+    }
+
+    public void DClicked(object sender, RoutedEventArgs e)
+    {
+        Numbers[4]++;
+        Numbers[5]++;
+        Numbers[7]++;
+        Numbers[8]++;
+    }
+    public void Reset(object sender, RoutedEventArgs e)
+    {
+        Numbers = new ObservableCollection<int>() { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    }
+}
